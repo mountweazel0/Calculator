4b4fc67ea985703feedb99990de903f6aa2696f5
mountweazel0 <belonogovavalentina@mail.ru>
2022-06-20 16:43:07 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    Calculator calc = Calculator.instance.get();
+
+    int a = calc.plus.apply(1, 2);
+    int b = calc.minus.apply(1,1);
+    int c = calc.devide.apply(a, b);
+
+    calc.println.accept(c);
+
+    
+}
